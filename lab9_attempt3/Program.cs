45b105dd82defcf6dd4b8d40d00a8abb70789282
Анастасия Зеленина <anastasiazelenina346@gmail.com>
2025-02-15 14:57:55 +0500

    Добавьте файлы проекта.

---
+++ lab9_attempt3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lab9_attempt3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ lab9_tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace lab9_tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
